7d1932697e03d02f7a450baae877b492d9871c1e
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-01-17 11:34:59 +0530

    note popover button created

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
 import PopoverComponent from "../../components/PopoverComponent";
+import ButtonComponent from "../../components/ButtonComponent";
 
 const Home = () => {
   useEffect(() => {
@@ -7,9 +9,14 @@ const Home = () => {
   });
 
   const popovercontent = [
-    <>
-      <h1>nemonic notes</h1>
-    </>
+    <div style={{width: '80%', height: '100%', display:'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+      <div style={{width: '35%', minWidth: '150px'}}>
+        <ButtonComponent customButtonStyle={{backgroundColor: 'black', display: 'flex', alignItems: 'center', justifyContent: 'space-around', margin: '0px !important'}} starticon={<Avatar><span style={{color: 'black'}}>N</span></Avatar>} buttontext={'Normal Notes'}/>
+      </div>
+      <div style={{width: '35%', minWidth: '150px'}}>
+        <ButtonComponent customButtonStyle={{backgroundColor: '#4F7BD2', display: 'flex', alignItems: 'center', justifyContent: 'space-around', margin: '0px !important'}} starticon={<Avatar><span style={{color: 'black'}}>M</span></Avatar>} buttontext={'Mnemonics Notes'}/>
+      </div>
+    </div>
   ]
 
   return (
