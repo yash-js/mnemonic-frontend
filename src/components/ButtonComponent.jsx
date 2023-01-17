0757b2346d1ecd86e90dfcec88443419554f526e
Yash <yash@mahitechllc.com>
2023-01-17 13:40:52 +0530

    Loader Issue fixed

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import '../styles/mui.css';
-import { CircularProgress } from '@mui/material';
+import React from "react";
+import Button from "@mui/material/Button";
+import "../styles/mui.css";
+import CircularProgress from "@mui/material/CircularProgress";
 
-function ButtonComponent({extraclass,onClick,buttontext, isLoading, disabled, color,customButtonStyle, starticon, endicon}) {
-    return (
-        <Button disabled={disabled} variant="contained" color={color} className={`buttoncomponent ${extraclass}`} startIcon={starticon} endIcon={endicon} onClick={onClick} style={customButtonStyle}>{isLoading === true ?   <CircularProgress color='"#4F7BD2"' /> : buttontext}</Button>
-    )
+function ButtonComponent({
+  extraclass,
+  onClick,
+  buttontext,
+  isLoading,
+  disabled,
+  color,
+  customButtonStyle,
+  starticon,
+  endicon,
+}) {
+  return (
+    <Button
+      disabled={disabled}
+      variant="contained"
+      color={color}
+      className={`buttoncomponent ${extraclass}`}
+      startIcon={starticon}
+      endIcon={endicon}
+      onClick={onClick}
+      style={customButtonStyle}
+    >
+      {isLoading === true ? <CircularProgress color="primary"  /> : buttontext}
+    </Button>
+  );
 }
 
-export default ButtonComponent
+export default ButtonComponent;
