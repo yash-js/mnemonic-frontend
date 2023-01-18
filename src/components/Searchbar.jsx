a0e9c2bf94442fa610799a2e96321633eb0ed027
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-01-18 22:01:42 +0530

    fix some minor typo

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Box } from "@mui/system";
 import InputField from "./InputField";
 import search from "../assets/images/search.svg";
