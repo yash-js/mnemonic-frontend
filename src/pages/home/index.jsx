73cb0e2a0de17857aa91c59d79a6d5b3311fed90
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-01-23 23:48:56 +0530

    fix normal popover not opening issue

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { Grid } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { setActivePopOver } from "../../features/popoverslice";
+import { useDispatch, useSelector } from "react-redux";
+import { setActivePopOver, getActivePopOver } from "../../features/popoverslice";
 import ButtonComponent from "../../components/ButtonComponent";
 import PopoverComponent from "../../components/PopoverComponent";
+import RichTextEditor from "../../components/RichTextEditor";
+import InputField from "../../components/InputField";
 import Avatar from "@mui/material/Avatar";
 import NoteCard from "../../components/NoteCard";
 import { createNote, getNotes } from "../../lib/API_Calls";
@@ -11,7 +13,12 @@ import { useState } from "react";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const activepopover = useSelector(getActivePopOver);
   const [homedisabled, setHomeDisabled] = React.useState(false);
+  const [noteTitle, setNoteTitle] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+  const onFocusField = () => setError({});
 
   useEffect(() => {
     return () => (document.title = "Mnemonic");
@@ -26,6 +33,59 @@ const Home = () => {
       setHomeDisabled(true);
     }
   };
+
+  const popovernormalcontent = [
+    <div className="notetopcontent normalnotebox">
+      <div className="normalnotesheading">
+        <h3>Normal Notes</h3>
+      </div>
+      <div className="normalnotescontent">
+        <RichTextEditor />
+      </div>
+    </div>
+  ]
+
+  const popovermnemoniccontent = [
+    <div className="notetopcontent mnemonicnotebox">
+      <div className="mnemonicnotesheading">
+        <h3>Mnemonic Notes</h3>
+      </div>
+      <div className="mnemonicnotescontent">
+        <h1>Mnemonic Notes</h1>
+      </div>
+    </div>
+  ]
+
+  const popovernotetitlecontent = [
+    <div className="notetopcontent notetitlebox">
+      <div className="notetitleheading">
+        <h3>Note Settings</h3>
+      </div>
+      <div className="notetitlecontent">
+        <Grid container spacing={2}>
+          <Grid item xs={12} className={'notedetails'}>
+            <h4>Note Details</h4>
+            <InputField
+                extraclass={"signupInput"}
+                type="text"
+                label="Note Title"
+                name="noteTitle"
+                value={noteTitle}
+                onChange={(e) => setNoteTitle(e.target.value)}
+                error={error && error.field === "noteTitle"}
+                errorText={
+                  error && error.field === "noteTitle" && error.error
+                }
+                onFocusField={onFocusField}
+              />
+          </Grid>
+          <Grid item xs={12} className={'notesharing'}>
+            <h4>Note Sharing</h4>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  ]
 
   const cardcontent = [
     {
@@ -137,12 +197,22 @@ const Home = () => {
               </Grid>
             ))}
           </Grid>
-          <PopoverComponent
-            btnname={"home"}
-            popoverclassname={"homecontentpopover"}
-            popovercontent={popovercontent}
-            popoverstate={homedisabled ? true : false}
-          />
+          {
+            activepopover === 'normal' ? (
+              <PopoverComponent popoverclassname={'normalnotes'} popovercontent={popovernormalcontent} richtext={true} popoverstate={true}/>
+            ) : activepopover === 'mnemonic' ? (
+              <PopoverComponent popoverclassname={'mnemonicnotes'} popovercontent={popovermnemoniccontent} popoverstate={true}/>
+            ) : activepopover === 'notetitle' ? (
+              <PopoverComponent popoverclassname={'notetitle'} popovercontent={popovernotetitlecontent} popoverstate={true}/>
+            ) : (
+                <PopoverComponent
+                btnname={"home"}
+                popoverclassname={"homecontentpopover"}
+                popovercontent={popovercontent}
+                popoverstate={homedisabled ? true : false}
+              />
+            )
+          }
         </div>
       </div>
     </>
