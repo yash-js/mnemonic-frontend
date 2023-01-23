a93100b3e4bf42a608033af911c7a9ac92643ca1
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-01-23 23:39:27 +0530

    added react quill editor

--- src/components/RichTextEditor.jsx
+++ src/components/RichTextEditor.jsx
@@ -1,41 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { EditorState, convertToRaw } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import draftToHtml from "draftjs-to-html";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import htmlToDraft from "html-to-draftjs";
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css'; 
 
 function RichTextEditor() {
-  let editorState = EditorState.createEmpty();
-  const [richtextdata, setRichtextdata] = useState(editorState);
-  const savedata = localStorage.getItem("notedata")
-    ? JSON.parse(localStorage.getItem("notedata"))
-    : richtextdata;
-  // const draftdata = htmlToDraft(savedata);
+  const { quill, quillRef } = useQuill();
+  const [value,setValue]=useState();
 
-  const onEditorStateChange = (editorState) => {
-    setRichtextdata(editorState);
-  };
+  useEffect(() => {
+    if (quill) {
+      quill.on('text-change', () => {
+        setValue(quillRef.current.firstChild.innerHTML)
+      });
+    }
+  }, [quill]);
 
   useEffect(() => {
     return () => {
       localStorage.setItem(
         "notedata",
-        JSON.stringify(
-          draftToHtml(convertToRaw(richtextdata.getCurrentContent()))
-        )
+        JSON.stringify(value)
       );
     };
-  }, [richtextdata]);
+  }, [value]);
 
-  return (
-    <div className="richtexteditor">
-      <Editor
-        editorState={richtextdata}
-        onEditorStateChange={onEditorStateChange}
-      />
-    </div>
+  return(
+      <div>
+          <div style={{ width: '100%', height: 'calc(100% - 42px)' }}>
+              <div ref={quillRef} />
+          </div>
+      </div>
   );
 }
 
