813d843cc0dd48adc77b60e850ec05b39dba5f26
Dharmendrakumar-Patel <pateldharmendrakumarsalamsinh@yahoo.com>
2023-01-25 15:54:32 +0530

    added Autocomplete logic

--- src/components/AutoCompleteComponent.jsx
+++ src/components/AutoCompleteComponent.jsx
@@ -1,47 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import Chip from '@mui/material/Chip';
+import { useSelector } from 'react-redux';
+import { userData } from "../features/userSlice";
+import { useEffect } from 'react';
 
 function AutoCompleteComponent() {
-    const top100Films = [
-        { title: 'The Shawshank Redemption', year: 1994 },
-        { title: 'The Godfather', year: 1972 },
-        { title: 'The Godfather: Part II', year: 1974 },
-        { title: 'The Dark Knight', year: 2008 },
-        { title: '12 Angry Men', year: 1957 },
-        { title: "Schindler's List", year: 1993 },
-        { title: 'Pulp Fiction', year: 1994 },
-        {
-          title: 'The Lord of the Rings: The Return of the King',
-          year: 2003,
-        },
-    ]
+  const user = useSelector(userData);
+  const friends = user && user?.friends ? user?.friends : [];
+  const [shareValue, setShareValue] = useState()
 
-    const [value, setValue] = React.useState();
+  useEffect(() => {
+    console.log('friend', shareValue)
+  }, [shareValue])
 
   return (
     <Autocomplete
-        multiple
-        id="fixed-tags-demo"
-        value={value}
-        onChange={(event, newValue) => {
-        setValue([newValue]);
-        }}
-        options={top100Films}
-        getOptionLabel={(option) => option.title}
-        renderTags={(tagValue, getTagProps) =>
-        tagValue.map((option, index) => (
-            <Chip
-            label={option.title}
-            {...getTagProps({ index })}
-            />
-        ))
-        }
-        style={{ width: '100%' }}
-        renderInput={(params) => (
-        <TextField {...params} label="Search Friends" placeholder="Favorites" />
-        )}
+      multiple
+      id="asynchronous-demo"
+      sx={{ width: '100%' }}
+      isOptionEqualToValue={(option, value) => option.username === value.username}
+      getOptionLabel={(option) => option.username}
+      options={friends}
+      onChange={(event, value) => setShareValue(value)}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search Friend"
+        />
+      )}
     />
   )
 }
