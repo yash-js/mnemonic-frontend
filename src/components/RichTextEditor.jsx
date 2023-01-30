b31eb020204d55c77f9f9c5998dbac927c897864
Yash <yash@mahitechllc.com>
2023-01-30 10:03:43 +0530

    Text editor fixes

--- src/components/RichTextEditor.jsx
+++ src/components/RichTextEditor.jsx
@@ -1,30 +1,34 @@
 import React, { useEffect } from "react";
-import { useQuill } from 'react-quilljs';
-import 'quill/dist/quill.snow.css'; 
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
 
-function RichTextEditor({setValue, value}) {
+function RichTextEditor({ setValue, value }) {
   const { quill, quillRef } = useQuill();
 
   useEffect(() => {
     if (quill) {
-      quill.on('text-change', () => {
-        setValue(quillRef.current.firstChild.innerHTML)
+      quill.on("text-change", () => {
+        setValue(quillRef.current.firstChild.innerHTML);
       });
     }
   }, [quill, setValue, quillRef, value]);
 
   useEffect(() => {
     if (quill) {
-      quill.clipboard.dangerouslyPasteHTML(value.length > 0 ? value : '<p><br></p>')
+      if (value && value.length > 0)
+        return (quillRef.current.firstChild.innerHTML = value);
+      else {
+        return (quillRef.current.firstChild.innerHTML = "<p><br></p>");
+      }
     }
-  },[quill])
+  }, [quill]);
 
-  return(
-      <div>
-          <div style={{ width: '100%', height: 'calc(100% - 42px)' }}>
-              <div ref={quillRef} />
-          </div>
+  return (
+    <div>
+      <div style={{ width: "100%", height: "calc(100% - 42px)" }}>
+        <div ref={quillRef} />
       </div>
+    </div>
   );
 }
 
